3fd77dfe4bfee3a9e3ea72e2fce2d893c92209ac
JakeKwak95 <wlghrhkr@gmail.com>
2025-09-06 21:11:20 +0900

    씬로딩 구현

--- Assets/01_Scripts/Managers/SceneChangeManager.cs
+++ Assets/01_Scripts/Managers/SceneChangeManager.cs
@@ -1,12 +1,18 @@
+using EditorAttributes;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
-    // 로딩 씬 이름을 지정하세요 (예: "LoadingScene")
     [SerializeField] private string loadingSceneName = "LoadingScene";
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [Button]
     public void LoadSceneAsync(int index)
     {
         StartCoroutine(LoadSceneAsyncCoroutine(index));
@@ -14,35 +20,29 @@ public class SceneChangeManager : MonoBehaviour
 
     private IEnumerator LoadSceneAsyncCoroutine(int index)
     {
-        // 1. 로딩 씬 먼저 로드 (싱글 모드로 전환)
         AsyncOperation loadingSceneOp = SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Single);
         while (!loadingSceneOp.isDone)
         {
             yield return null;
         }
 
-        // 2. 실제 로드할 씬 비동기로 로드 (Additive 모드)
         AsyncOperation targetSceneOp = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
         targetSceneOp.allowSceneActivation = false;
 
-        // 3. 로딩 진행률 체크 (로딩씬에서 진행률 표시 가능)
         while (targetSceneOp.progress < 0.9f)
         {
-            // 필요시 진행률 전달
+            print(targetSceneOp.progress);
             yield return null;
         }
 
-        // 4. 실제 씬 활성화
         targetSceneOp.allowSceneActivation = true;
         while (!targetSceneOp.isDone)
         {
             yield return null;
         }
 
-        // 5. 로딩씬 언로드
         SceneManager.UnloadSceneAsync(loadingSceneName);
 
-        // 6. 타겟 씬을 활성화 (필요시)
         Scene targetScene = SceneManager.GetSceneByBuildIndex(index);
         if (targetScene.IsValid())
         {
